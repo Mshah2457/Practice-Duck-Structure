8424102d64fd4b0b34b76c61a860a886c528574c
DhrutiUpadhyay <37285944+DhrutiUpadhyay@users.noreply.github.com>
2018-06-14 12:42:55 +0530

    second try

--- src/TemplateFeature/components/sampleDumbComponent/index.tsx
+++ src/TemplateFeature/components/sampleDumbComponent/index.tsx
@@ -35,7 +35,7 @@ export default class SampleDumbComponent extends PureComponent<Props, State> {
                     {`BAR: ${bar}`}
                 </Text>
                 <Text>
-                   ghghfhhfgh {`BAZ: ${baz}`}
+                    {`BAZ: ${baz}`}
                 </Text>
             </View>
         );
